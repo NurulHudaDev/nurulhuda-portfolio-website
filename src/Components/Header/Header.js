2b011ffb78777735679f0a8a95bf24bdea71c71a
NurulHudaDev <nurulhuda22dev@gmail.com>
2023-08-24 17:02:10 +0600

    responsive menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,43 +1,48 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useState } from 'react';
 import './Header.css';
 import { NavContext } from '../Contex/NavContext';
-import { FaBars, FaTimes } from "react-icons/fa"
+import { FaBars, FaThLarge, FaTimes } from "react-icons/fa"
 
 const Header = () => {
 
-    const { activeLinkId } = useContext(NavContext);
+  const { activeLinkId } = useContext(NavContext);
 
-    const navLinks = ['Home', 'About', 'Projects', 'Contact'];
+  const navLinks = ['Home', 'About', 'Projects', 'Contact'];
 
-    const handleClickLogo = () => {
-        document.getElementById("homeSection").scrollIntoView({ behavior: "smooth" })
-      }
+  const handleClickLogo = () => {
+    document.getElementById("homeSection").scrollIntoView({ behavior: "smooth" })
+  }
 
-    const renderNavLink = (content) => {
+  const renderNavLink = (content) => {
 
-        const scrollToId = `${content.toLowerCase()}Section`;
+    const scrollToId = `${content.toLowerCase()}Section`;
 
-        const handleClickNav =() => {
-            document.getElementById(scrollToId).scrollIntoView({ behavior: 'smooth'})
-        }
-        return (
-            <ul key={content}>
-                <button onClick={handleClickNav} className={activeLinkId === content ? 'active' : ""}>{content}</button>
-            </ul>
-        )
+    const handleClickNav = () => {
+      document.getElementById(scrollToId).scrollIntoView({ behavior: 'smooth' })
     }
+    return (
+      <ul key={content}>
+        <button onClick={handleClickNav} className={activeLinkId === content ? 'active' : ""}>{content}</button>
+      </ul>
+    )
+  }
+
+  const [Toggle, showMenu] = useState(false);
 
   return (
-    <header className='navbar'>
-     <div className='container'>
-        <div className='navContainer'>
-           <h1 onClick={handleClickLogo}>LOGO</h1>
-           <nav className='nav'>
-                {navLinks.map(nav => renderNavLink(nav))}
-           </nav>
-        </div>
-     </div>
-    </header>
+    <nav className='navbar'>
+      <button className='nav-btn nav-toggle' onClick={() => showMenu (!Toggle)}>
+        <FaThLarge />
+      </button>
+      <h1 className='logo' onClick={handleClickLogo}>LOGO</h1>
+      <div className={Toggle ? 'nav-links show-menu' : 'nav-links'}>
+        {navLinks.map(nav => renderNavLink(nav))}
+        <button className='nav-btn nav-close-btn' onClick={() => showMenu (!Toggle)}>
+        <FaTimes />
+      </button>
+      </div>
+      
+    </nav>
   );
 };
 
