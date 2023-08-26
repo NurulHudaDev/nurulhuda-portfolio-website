6d1cbd1205db12b9f3dd89c9a093b26e79fe92ed
NurulHudaDev <nurulhuda22dev@gmail.com>
2023-08-26 16:24:15 +0600

    services-component-update

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Services.css';
 import { useNav } from '../Hooks/useNav';
-import { HiCode } from "react-icons/hi";
+import { FaCode, FaPager, FaPenNib } from "react-icons/fa6";
 
 const Services = () => {
 
@@ -10,29 +10,38 @@ const Services = () => {
   return (
     <section ref={servicesRef} id='servicesSection' className='services'>
       <h1>Services</h1>
-        <div class="row">
-          <div class="course-col">
-            <h2>Web Development</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.</p>
+      <div class="row">
+        <div class="course-col">
+          <div className='icon-title'>
+          <div className='icon'><FaCode /></div>
+          <h2>Web Development</h2>
           </div>
-          <div class="course-col">
-            <h2>UI/UX Design</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.</p>
-          </div>
-          <div class="course-col">
-            <h2>Logo Design</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.</p>
-          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.</p>
         </div>
+        <div class="course-col">
+        <div className='icon-title'>
+          <div className='icon'><FaPager /></div>
+          <h2>UI/UX Design</h2>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="course-col">
+        <div className='icon-title'>
+          <div className='icon'><FaPenNib /></div>
+          <h2>Logo Design</h2>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
     </section>
   );
 };
