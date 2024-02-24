332d1c6d925bd5fbf85a96f38bc19833bcab7ded
NurulHudaDev <nurulhuda22dev@gmail.com>
2024-02-24 21:33:24 +0600

    responsive-problem-fixed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,27 +13,29 @@ const Home = () => {
   return (
     <div ref={homeRef} id='homeSection' className='banner'>
       <div className='banner_overlay'>
-      <div className='top_banner'>
-        <div data-aos="zoom-out-right" className='banner_left_site'>
-          <h1 className='name'>I'M NURUL HUDA</h1>
-          <h2 className='title'>Front-end web developer</h2>
-          <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <div className='resume_cv_btn'>
-            <button className='resume_btn'>Resume</button>
-            <button className='cv_btn'>CV</button>
+        <div className='top_banner'>
+          <div data-aos="zoom-out-right" className='banner_left_site'>
+            <div>
+              <h1 className='name'>I'M NURUL HUDA</h1>
+              <h2 className='title'>Front-end web developer</h2>
+              <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <div className='resume_cv_btn'>
+                <button className='resume_btn'>Resume</button>
+                <button className='cv_btn'>CV</button>
+              </div>
+            </div>
+            <div className='social_icon'>
+              <a href='https://www.facebook.com/nurulhuda.dav' alt=''><FaFacebookSquare /></a>
+              <a href='https://www.instagram.com/nurulhuda_dev/' alt=''><FaInstagramSquare /></a>
+              <a href='' alt=''><FaTwitterSquare /></a>
+              <a href='https://www.linkedin.com/in/nurul-huda-d/' alt=''><FaLinkedin /></a>
+              <a href='https://github.com/NurulHudaDev' alt=''><FaGithubSquare /></a>
+            </div>
           </div>
-          <div className='social_icon'>
-            <a href='https://www.facebook.com/nurulhuda.dav' alt=''><FaFacebookSquare /></a>
-            <a href='https://www.instagram.com/nurulhuda_dev/' alt=''><FaInstagramSquare /></a>
-            <a href='' alt=''><FaTwitterSquare /></a>
-            <a href='https://www.linkedin.com/in/nurul-huda-d/' alt=''><FaLinkedin /></a>
-            <a href='https://github.com/NurulHudaDev' alt=''><FaGithubSquare /></a>
+          <div className='banner_right_site'>
+            <img data-aos="zoom-out-left" className='banner_image' src={BannerImage2} alt="" srcset="" />
           </div>
         </div>
-        <div className='banner_right_site'>
-          <img data-aos="zoom-out-left" className='banner_image' src={BannerImage2} alt="" srcset="" />
-        </div>
-      </div>
       </div>
     </div>
   );
